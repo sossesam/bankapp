a57df5e298df665eb1b487b6ff49f1381dd3cef7
sossesam <sossesam@gmail.com>
2021-12-15 07:23:26 +0100

    bank app

--- bankapp.js
+++ bankapp.js
@@ -1,1 +1,33 @@
-bankapp
+alert("working")
+
+
+function btn1(){
+  alert("working")  ;
+};
+function btn2(){
+    var bal = 50000
+    var w = document.getElementById("cash").value;
+    document.getElementById("balance").innerHTML = "Your  account balance is N" + bal;
+    var a = "sanmi"; 
+    var b = "april";
+    document.getElementById("greet").innerHTML = "Welcome " + a;
+    
+    c = "sanmi";
+    d = "april";
+
+    if(a === c && b ===d){
+        if ( bal > w && bal > 0){
+          var bal = bal - w;
+             document.getElementById("current").innerHTML = "Current Balance: N" + bal;
+            
+         }
+         else{
+            document.getElementById("current").innerHTML = "insuffient fund "+ " Current Balance: N";
+            
+         }
+        }else{
+        alert("wrong username or password")
+    }
+    
+    
+}
